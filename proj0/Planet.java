fa5320859978255b670e7397c4fd75ba2e4c185d
AlanMusk <alanmusk@163.com>
2023-09-21 08:49:34 +0800

    Somethings

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -17,7 +17,12 @@ public class Planet {
     }
     public Planet(Planet p)
     {
-        Planet p_copy=new Planet(p.xxPos,p.yyPos,p.xxVel,p.yyVel,p.mass,p.imgFileName);
-
+        xxPos=p.xxPos;
+        yyPos=p.yyPos;
+        xxVel=p.xxVel;
+        yyVel=p.yyVel;
+        mass=p.mass;
+        imgFileName=p.imgFileName;
     }
+    // calculate the distance of chose planet
 }
