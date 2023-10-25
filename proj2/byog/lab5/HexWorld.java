38720f8836c92dcf97aa9729277c1c5098484787
AlanMusk <alanmusk@163.com>
2023-10-26 00:01:07 +0800

    lab05

--- proj2/byog/lab5/HexWorld.java
+++ proj2/byog/lab5/HexWorld.java
@@ -6,10 +6,23 @@ import byog.TileEngine.TERenderer;
 import byog.TileEngine.TETile;
 import byog.TileEngine.Tileset;
 
+import java.awt.*;
 import java.util.Random;
 
 /**
  * Draws a world consisting of hexagonal regions.
  */
 public class HexWorld {
+    private static final int WIDTH = 60;
+    private static final int HEIGHT = 30;
+
+    public void addHexagon(int side,int x,int y)
+    {
+
+        TETile a=new TETile('a', Color.white,Color.BLACK,"Task");
+    }
+
+    public static void main(String[] args) {
+        
+    }
 }
