6ee16671e962b8008f0d62e2f01abe18675e4d57
AlanMusk <alanmusk@163.com>
2023-10-26 12:14:22 +0800

    Updata

--- proj2/byog/lab5/HexWorld.java
+++ proj2/byog/lab5/HexWorld.java
@@ -16,13 +16,27 @@ public class HexWorld {
     private static final int WIDTH = 60;
     private static final int HEIGHT = 30;
 
-    public void addHexagon(int side,int x,int y)
+    public static void addHexagon(TETile[][] tl,int side,int x,int y)
     {
 
         TETile a=new TETile('a', Color.white,Color.BLACK,"Task");
+        tl[x][y]=a;
     }
 
     public static void main(String[] args) {
-        
+        // initialize the tile rendering engine with a window of size WIDTH x HEIGHT
+        TERenderer ter = new TERenderer();
+        ter.initialize(WIDTH, HEIGHT);
+
+        // initialize tiles
+        TETile[][] world = new TETile[WIDTH][HEIGHT];
+        for (int x = 0; x < WIDTH; x += 1) {
+            for (int y = 0; y < HEIGHT; y += 1) {
+                world[x][y] = Tileset.NOTHING;
+            }
+        }
+        addHexagon(world,2,10,10);
+        // draws the world to the screen
+        ter.renderFrame(world);
     }
 }
