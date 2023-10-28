580321dcbf5904c5f8af24f0bce220538468c8b8
AlanMusk <alanmusk@163.com>
2023-10-28 15:11:19 +0800

    Lab05

--- proj2/byog/lab5/HexWorld.java
+++ proj2/byog/lab5/HexWorld.java
@@ -13,30 +13,65 @@ import java.util.Random;
  * Draws a world consisting of hexagonal regions.
  */
 public class HexWorld {
-    private static final int WIDTH = 60;
-    private static final int HEIGHT = 30;
-
-    public static void addHexagon(TETile[][] tl,int side,int x,int y)
+    public static void addHexagon(TETile[][] world,Position p,int s,TETile t)
     {
 
-        TETile a=new TETile('a', Color.white,Color.BLACK,"Task");
-        tl[x][y]=a;
-    }
-
-    public static void main(String[] args) {
-        // initialize the tile rendering engine with a window of size WIDTH x HEIGHT
-        TERenderer ter = new TERenderer();
-        ter.initialize(WIDTH, HEIGHT);
-
-        // initialize tiles
-        TETile[][] world = new TETile[WIDTH][HEIGHT];
-        for (int x = 0; x < WIDTH; x += 1) {
-            for (int y = 0; y < HEIGHT; y += 1) {
-                world[x][y] = Tileset.NOTHING;
-            }
+        if(s<2)
+        {
+            throw new IllegalArgumentException("Hexagon must be at least size 2.");
         }
-        addHexagon(world,2,10,10);
-        // draws the world to the screen
-        ter.renderFrame(world);
+        for(int yi=0;yi<2*s;yi+=1)
+        {
+            int thisRowY=p.getY()+yi;
+            int xRowStart=p.getX()+hexRowOffset(s,yi);
+            Position rowStartP=new Position(xRowStart,thisRowY);
+            int rowWidth=hexRowWidth(s,yi);
+            addRow(world,rowStartP,rowWidth,t);
+        }
     }
+
+    public static int hexRowWidth(int s,int i)
+    {
+        int effectiveI=i;
+        if(i>=s)
+        {
+            effectiveI=2*s-1-effectiveI;
+        }
+        return s+s*effectiveI;
+    }
+    public static int hexRowOffset(int s,int i)
+    {
+        int effectiveI=i;
+        if(i>=s)
+        {
+            effectiveI=2*s-1-effectiveI;
+        }
+        return -effectiveI;
+    }
+
+    public static void addRow(TETile[][] world, Position p,int width,TETile t)
+    {
+        for(int xi=0;xi<width;xi+=1)
+        {
+            int xCoord=p.getX()+xi;
+            int yCoord=p.getY();
+            world[xCoord][yCoord]=TETile.colorVariant(t,32,32,32,new Random());
+        }
+    }
+
+    @Test
+    public void testHexRowWidth()
+    {
+        assertEquals(3, hexRowWidth(3, 5));
+        assertEquals(5, hexRowWidth(3, 4));
+        assertEquals(7, hexRowWidth(3, 3));
+        assertEquals(7, hexRowWidth(3, 2));
+        assertEquals(5, hexRowWidth(3, 1));
+        assertEquals(3, hexRowWidth(3, 0));
+        assertEquals(2, hexRowWidth(2, 0));
+        assertEquals(4, hexRowWidth(2, 1));
+        assertEquals(4, hexRowWidth(2, 2));
+        assertEquals(2, hexRowWidth(2, 3));
+    }
+
 }
